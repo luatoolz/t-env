99858b2fe8bc106b4f97018dda960b5742bf2e56
luatoolz <luatoolz@somahorse.com>
2024-08-12 14:25:53 +0300

    fix typo

--- lua/t/env.lua
+++ lua/t/env.lua
@@ -10,7 +10,7 @@ return setmetatable({}, {
   __call = function(self, it) if type(it)=='table' then for k,v in pairs(it) do fallback[k]=v end end end,
   __newindex = function(self, k, v) if ok(k) then default[k]=v end end,
   __index = function(self, k) if ok(k) then return os.getenv(k) or default[k] or fallback[k] end end,
-  __sub = function(self, it) if ok(it) then fallback[k]=nil end;
+  __sub = function(self, it) if ok(it) then fallback[it]=nil end;
     if type(it)=='table' then for _,k in ipairs(it) do fallback[k]=nil end end
   end,
 })
